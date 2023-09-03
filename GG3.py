a68460d10ff784cf6210a883bb2906360f492c43
Greatabc <36228455+Greatabc@users.noreply.github.com>
2023-09-03 15:49:28 +0300

    Add files via upload

---
+++ GG3.py
@@ -0,0 +1,4 @@
+
+import streamlit as st
+# Text
+st.text("Hello GeeksForGeeks!!!")
---
+++ GG4.py
@@ -0,0 +1,6 @@
+import streamlit as st
+# Markdown
+st.markdown("### This is a markdown")
+st.markdown("## This is a markdown")
+st.markdown("# This is a markdown")
+st.markdown("This is a markdown")
