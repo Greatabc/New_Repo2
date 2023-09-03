8b0305007e8452a71bdbb437e55150e9dee54285
Greatabc <36228455+Greatabc@users.noreply.github.com>
2023-09-03 15:56:06 +0300

    Add files via upload

---
+++ GG6.py
@@ -0,0 +1,7 @@
+
+import streamlit as st
+# Write text
+st.write("Text with write")
+
+# Writing python inbuilt function range()
+st.write(range(10))
---
+++ GG7.py
@@ -0,0 +1,11 @@
+
+import streamlit as st
+# Display Images
+
+# checkbox
+# check if the checkbox is checked
+# title of the checkbox is 'Show/Hide'
+if st.checkbox("Show/Hide"):
+
+	# display the text if the checkbox returns True value
+	st.text("Showing the widget")
