c037144ee8857fe5da765122bbd30f35e715856d
Greatabc <36228455+Greatabc@users.noreply.github.com>
2023-10-14 18:54:01 +0300

    Update Plot_pinguin_New.py

--- Plot_pinguin_New.py
+++ Plot_pinguin_New.py
@@ -3,8 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import os
-os.chdir('C:\\Users\\Sarwono\\PycharmProjects\\pythonProject1')
+
 
 
 st.title('Plot of Pinguin')
